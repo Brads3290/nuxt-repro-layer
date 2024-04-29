84e069f17c01feff64c78ef656c3489b3097ce78
Brad Sullivan <brad@sullivandigital.com.au>
2024-04-29 12:58:53 +1000

    initial

---
+++ components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { joinURL } from 'ufo'
+const { myLayer } = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello World!</h1>
+    <pre>{{ joinURL('a', '/b') }}</pre>
+  </div>
+</template>
