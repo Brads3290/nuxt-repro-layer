3aa1a50f70d5557a1a1e01691fa6487fc09dc7b7
Brad Sullivan <brad@sullivandigital.com.au>
2024-04-29 14:36:04 +1000

    Add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon', '@vueuse/nuxt'],
+  modules: ['nuxt-icon', '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
   alias: {
     lego: currentDir,
   },
